5f61c7a57c1baf90843961e3636e712c2cd3fcec
Achmad Susmono <149119012+4chmad5usmono@users.noreply.github.com>
2024-09-08 21:20:22 +0700

    add dlug

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,17 +14,46 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => [
         [
+            'id' =>1,
+            'slug' => 'judul-artikel-1',
             'title' =>'Judul Artikel 1',
             'author' => 'Achmad Susmono',
             'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo quos atque adipisci asperiores quo quas inventore beatae facere earum nostrum vitae sunt deserunt, labore voluptates laudantium quia quisquam amet numquam!'
         ],
         [
+            'id' =>2,
+            'slug' => 'judul-artikel-2',
             'title' =>'Judul Artikel 2',
             'author' => 'Achmad Susmono',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, ullam tenetur molestiae explicabo natus enim aliquam commodi cupiditate, saepe odit ducimus aperiam deleniti. At, possimus doloremque hic nam distinctio iste.'
         ]
     ] ]);
 });
+
+Route::get('/posts/{slug}', function($slug) {
+     $posts=[
+        [
+            'id' =>1,
+            'slug' => 'judul-artikel-1',
+            'title' =>'Judul Artikel 1',
+            'author' => 'Achmad Susmono',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo quos atque adipisci asperiores quo quas inventore beatae facere earum nostrum vitae sunt deserunt, labore voluptates laudantium quia quisquam amet numquam!'
+        ],
+        [
+            'id' =>2,
+            'slug' => 'judul-artikel-2',
+            'title' =>'Judul Artikel 2',
+            'author' => 'Achmad Susmono',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, ullam tenetur molestiae explicabo natus enim aliquam commodi cupiditate, saepe odit ducimus aperiam deleniti. At, possimus doloremque hic nam distinctio iste.'
+        ]
+        ];
+
+    $post =Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    } );
+
+    return view('post',['title' => 'Single Post', 'post' => $post ]);
+} );
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
